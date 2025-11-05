d7b4a6ff0f0b1dde1e6a0bbb446dc2f2019e07ea
Bebecu <103565394+Bebecu@users.noreply.github.com>
2025-11-05 17:53:33 +0100

    Add files via upload

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -12,8 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const imagenes = document.querySelectorAll(".gallery img");
     imagenes.forEach(img => { /* voy pasando por cada elemento para que haga una acción en cada una */
       img.addEventListener("mouseover", () => { /* al pasar el mouse */
-        imagenes.forEach(i => i.style.zIndex = "0"); /* reseteamos la posición de cada imágen para que no haya problema con la superposición */
-        img.style.transform = "scale(1.4)"; /* escalo la imagen en este tamaño */
+        img.style.transform = "scale(2.2)"; /* escalo la imagen */
         img.style.transition = "transform 0.4s"; /* tiempo que tarda en escalar */
         img.style.zIndex = "10"; /* hace que esté por encima de las demás */
         img.style.position = "relative"; /* necesario para que z-index funcione */
@@ -91,6 +90,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   }
 });
-
-
-
